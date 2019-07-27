0ac686048b7541b7189cf683980848736e97ebbb
Keith Williams Jr <keithwilliamsjr@gmail.com>
2019-07-27 10:04:15 -0400

    add update todo handler

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,60 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  var updatedTodoItem = {
+    TableName: todosTable,
+    Key: {
+      todoId: todoId
+    },
+    UpdateExpression: 'set name = :n, dueDate = :dd, done = :d',
+    ExpressionAttributeValues: {
+      ':n': updatedTodo.name,
+      ':dd': updatedTodo.dueDate,
+      ':d': updatedTodo.done
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }
+  console.log('updating item', updatedTodo)
+
+  try {
+    await docClient.update(updatedTodoItem).promise()
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        updatedTodo
+      })
+    }
+  } catch (error) {
+    console.log('Failed to update todo ', error)
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  }
 }
