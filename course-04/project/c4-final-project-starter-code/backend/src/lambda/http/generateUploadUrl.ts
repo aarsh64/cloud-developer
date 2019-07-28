7b8c496789154c58cb3ade5861226012888b0904
Keith Williams Jr <keithwilliamsjr@gmail.com>
2019-07-27 21:50:02 -0400

    add attachments url handler

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,37 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const attachementsTable = process.env.ATTACHMENTS_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
   return undefined
 }
