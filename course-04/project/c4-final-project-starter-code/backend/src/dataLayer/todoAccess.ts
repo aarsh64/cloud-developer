01abd258894edde0e4acbba39c3929218eec0019
Keith Williams Jr <keithwilliamsjr@gmail.com>
2019-07-31 07:47:02 -0400

    refactor business logic and data access helpers

---
+++ course-04/project/c4-final-project-starter-code/backend/src/dataLayer/todoAccess.ts
@@ -0,0 +1,67 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { DocumentClient, UpdateItemInput } from 'aws-sdk/clients/dynamodb'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+import { TodoItem } from '../models/TodoItem'
+
+export class TodosAccess {
+  constructor(
+    private readonly docClient: DocumentClient = createDynamoDBClient(),
+    private readonly todosTable = process.env.TODOS_TABLE,
+    private readonly userIdIndex = process.env.USER_ID_INDEX
+  ) {}
+
+  async getUserTodos(userId: string): Promise<TodoItem[]> {
+    const result = await this.docClient
+      .query({
+        TableName: this.todosTable,
+        IndexName: this.userIdIndex,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+          ':userId': userId
+        },
+        ScanIndexForward: false
+      })
+      .promise()
+
+    return result.Items as TodoItem[]
+  }
+
+  async createTodo(newTodoItem: TodoItem) {
+    return await this.docClient
+      .put({
+        TableName: this.todosTable,
+        Item: newTodoItem
+      })
+      .promise()
+  }
+
+  async deleteTodo(todoId: string) {
+    await this.docClient
+      .delete({
+        Key: {
+          todoId: todoId
+        },
+        TableName: this.todosTable
+      })
+      .promise()
+  }
+
+  async updateTodoItem(updatedTodoItem: UpdateItemInput) {
+    return this.docClient.update(updatedTodoItem).promise()
+  }
+}
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('Creating a local DynamoDB instance')
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+
+  return new XAWS.DynamoDB.DocumentClient()
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -1,40 +1,21 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-
-const todosTable = process.env.TODOS_TABLE
+import { updateTodo } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-    const updatedTodoItem = {
-      TableName: todosTable,
-      Key: {
-        todoId: todoId
-      },
-      ExpressionAttributeNames: { '#n': 'name' },
-      UpdateExpression: 'set #n = :n, dueDate = :dd, done = :d',
-      ExpressionAttributeValues: {
-        ':n': updatedTodo.name,
-        ':dd': updatedTodo.dueDate,
-        ':d': updatedTodo.done
-      },
-      ReturnValues: 'UPDATED_NEW'
-    }
-    console.log('updating item', updatedTodo)
+    console.log('updating item request', updatedTodo)
 
     try {
-      await docClient.update(updatedTodoItem).promise()
+      await updateTodo(todoId, updatedTodo)
 
       return {
         statusCode: 201,
